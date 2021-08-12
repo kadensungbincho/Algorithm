7254542c96c368c1f0441e67dbb8c8e478ba2958
조성빈(Sungbin Cho)/Data Platform팀/11ST <sbcho@sk.com>
2021-08-12 22:38:47 +0900

    add a solution

---
+++ hackerrank/problems/src/sorting/activityNotifications/Solution.java
@@ -0,0 +1,99 @@
+package sorting.activityNotifications;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+// https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem
+class Result {
+    /*
+     * Complete the 'activityNotifications' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY expenditure
+     *  2. INTEGER d
+     */
+
+    // answer from https://www.hackerrank.com/challenges/fraudulent-activity-notifications/forum/comments/499133
+    static int activityNotifications(List<Integer> list, int d) {;
+        Integer[] expenditure = new Integer[list.size()];
+        expenditure = list.toArray(expenditure);
+
+        int notificationCount = 0;
+
+        int[] data = new int[201];
+        for (int i = 0; i < d; i++) {
+            data[expenditure[i]]++;
+        }
+
+        for (int i = d; i < expenditure.length; i++) {
+
+            double median = getMedian(d, data);
+
+            if (expenditure[i] >= 2 * median) {
+                notificationCount++;
+
+            }
+
+            data[expenditure[i]]++;
+            data[expenditure[i - d]]--;
+
+        }
+
+        return notificationCount;
+    }
+
+    private static double getMedian(int d, int[] data) {
+        double median = 0;
+        if (d % 2 == 0) {
+            Integer m1 = null;
+            Integer m2 = null;
+            int count = 0;
+            for (int j = 0; j < data.length; j++) {
+                count += data[j];
+                if (m1 == null && count >= d/2) {
+                    m1 = j;
+                }
+                if (m2 == null && count >= d/2 + 1) {
+                    m2 = j;
+                    break;
+                }
+            }
+            median = (m1 + m2) / 2.0;
+        } else {
+            int count = 0;
+            for (int j = 0; j < data.length; j++) {
+                count += data[j];
+                if (count > d/2) {
+                    median = j;
+                    break;
+                }
+            }
+        }
+        return median;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> expenditure = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        bufferedReader.close();
+
+        int result = Result.activityNotifications(expenditure, d);
+
+        System.out.println(result);
+    }
+}
